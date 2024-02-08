051e2eb0228688efd2d287cc28b0e9cd01c5823a
wonde <wonde@AFFANSHAIKH>
2024-02-08 12:34:43 -0500

    I updated the readme.txt file

--- MvcMovie/Models/Movie.cs
+++ MvcMovie/Models/Movie.cs
@@ -12,7 +12,8 @@ public class Movie
     [Required]
     public string? Title { get; set; }
 
-    [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+    [Display(Name = "Release Date")]
+    [DataType(DataType.Date)]
     public DateTime ReleaseDate { get; set; }
 
     [Range(1, 100)]
